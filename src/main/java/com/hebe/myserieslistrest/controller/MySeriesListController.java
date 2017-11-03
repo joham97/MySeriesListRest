936e2b597ff19ad9daf2e2c9bee634e95aaf2b60
joham97 <joham97@web.de>
2017-11-03 15:01:06 +0100

    GetSerieDetails GetEpisodes

--- src/main/java/com/hebe/myserieslistrest/controller/MySeriesListController.java
+++ src/main/java/com/hebe/myserieslistrest/controller/MySeriesListController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.omertron.thetvdbapi.TheTVDBApi;
 import com.omertron.thetvdbapi.TvDbException;
+import com.omertron.thetvdbapi.model.Episode;
 import com.omertron.thetvdbapi.model.Series;
 
 @RestController
 public class MySeriesListController {
     
 	private static TheTVDBApi api = new TheTVDBApi("B2FABF16E0600F2C");
+	
+	private static final String language = "en";
 	
     /*
      * Search
@@ -23,7 +26,7 @@ public class MySeriesListController {
     @RequestMapping("/search")
     public List<Series> getSearch(@RequestParam(value="keywords", defaultValue="") String keywords) {
     	try {
-    		List<Series> series = api.searchSeries(keywords, "en");
+    		List<Series> series = api.searchSeries(keywords, language);
 	    	if(series.size() < 10){
 	    		for(int i = 0; i < series.size(); i++){
 	    			series.set(i, api.getSeries(series.get(i).getId(), series.get(i).getLanguage()));
@@ -47,6 +50,34 @@ public class MySeriesListController {
 			e.printStackTrace();
 		}
 		return null;
+    }
+    
+    /*
+     * Series by Id
+     */
+
+    @RequestMapping("/series")
+    public Series getSeriesById(@RequestParam(value="id", defaultValue="") String id) {
+    	try {
+			return api.getSeries(id, language);
+		} catch (TvDbException e) {
+			e.printStackTrace();
+		}
+    	return null;
+    }
+    
+    /*
+     * Episodes of Series
+     */
+
+    @RequestMapping("/series/episodes")
+    public List<Episode> getEpisodesOfSeries(@RequestParam(value="id", defaultValue="") String id) {
+    	try {
+			return api.getAllEpisodes(id, language);
+		} catch (TvDbException e) {
+			e.printStackTrace();
+		}
+    	return null;
     }
     
 }
